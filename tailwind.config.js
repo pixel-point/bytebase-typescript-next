1c69a44ae8eb21c344dc40d795e1bf14d3ae8478
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-04-03 12:44:58 +0400

    chore: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,21 +15,49 @@ module.exports = {
       inherit: colors.inherit,
       current: colors.current,
       transparent: colors.transparent,
-      // FIXME: Add colors
-      // Make sure that they are prepared in the Figma and follow the naming primary/secondary/gray-${number}
-      // Example of correctly prepared colors in Figma — https://user-images.githubusercontent.com/20713191/143586876-5e834233-9639-4166-9811-b00e63820d98.png
-      // Example of incorrectly prepared colors in Figma — https://user-images.githubusercontent.com/20713191/143586974-6986149f-aee3-450c-a1dd-26e73e3aca02.png
-      // black: '',
-      // white: '',
-      // primary: {
-      //   1: '',
-      // },
-      // secondary: {
-      //   1: '',
-      // },
-      // gray: {
-      //   1: '',
-      // },
+      white: colors.white,
+      black: '#0F1624',
+      primary: {
+        1: '#5647EB',
+        2: '#32279B',
+      },
+      secondary: {
+        1: '#FFE666',
+        2: '#1FD68A',
+        3: '#3DB8F5',
+        4: '#EB477E',
+        5: '#EC93EC',
+      },
+      gray: {
+        15: '#172136',
+        30: '#364563',
+        40: '#4D5E80',
+        50: '#6A7895',
+        60: '#8893AA',
+        70: '#A7AFBE',
+        80: '#C4C9D4',
+        90: '#E2E4E9',
+        94: '#EDEFF2',
+        97: '#F6F7F8',
+      },
+      tones: {
+        'purple-light': '#F0F2FF',
+        'purple-dark': '#ACB2D2',
+        'blue-light': '#F0FAFE',
+        'blue-dark': '#9CBAC9',
+        'green-light': '#F2FCF8',
+        'green-dark': '#9CC9B6',
+        'yellow-light': '#FEFEE1',
+        'yellow-dark': '#E2E29C',
+        'pink-light': '#FDF2FD',
+        'pink-dark': '#D2ACD2',
+      },
+      code: {
+        blue: '#192AE6',
+      },
+      tags: {
+        purple: '#382E9E',
+      },
     }),
     screens: {
       '2xl': { max: '1919px' },
