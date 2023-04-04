fe7f4c786f9983ec4c84a69df6fea029430ba376
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-04-04 11:43:05 +0400

    feat: add link component

---
+++ src/components/shared/link/link.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import NextLink from 'next/link';
+
+import { FC } from 'react';
+
+import clsx from 'clsx';
+
+const styles = {
+  transition: 'transition-colors duration-200',
+  base: 'inline-flex leading-none items-center',
+  size: {
+    md: 'text-16',
+  },
+  theme: {
+    'gray-15': 'text-gray-15 hover:text-primary-1 disabled:text-gray-60',
+  },
+};
+
+type LinkProps = {
+  additionalClassName?: string;
+  to: string;
+  size?: keyof typeof styles.size;
+  theme?: keyof typeof styles.theme;
+  children: React.ReactNode;
+  withArrow?: boolean;
+};
+
+const Link: FC<LinkProps> = ({
+  additionalClassName = null,
+  size = null,
+  theme = null,
+  to,
+  children,
+  withArrow = false,
+  ...props
+}) => {
+  const linkClassName = clsx(
+    styles.transition,
+    size && theme && styles.base,
+    size && styles.size[size],
+    theme && styles.theme[theme],
+    additionalClassName,
+  );
+
+  const content = <>{withArrow ? <span>{children}</span> : children}</>;
+
+  if (to.startsWith('/')) {
+    return (
+      <NextLink className={linkClassName} href={to} {...props}>
+        {content}
+      </NextLink>
+    );
+  }
+
+  return (
+    <a className={linkClassName} href={to} {...props}>
+      {content}
+    </a>
+  );
+};
+
+export default Link;
