cc8eb0680c1ffd991b238f6155a470ed07b382f4
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-04-05 18:09:21 +0400

    feat: add icons to footer links and check pixel perfect

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -1,3 +1,6 @@
+import ExternalIcon from '@/svgs/external.inline.svg';
+import GithubIcon from '@/svgs/github.inline.svg';
+
 import Link from '@/components/shared/link';
 
 import { MENUS } from '@/lib/menus.js';
@@ -22,23 +25,31 @@ const socialLinks = [
   },
 ];
 
-//TODO: add external link icons
 const Footer = () => (
   <footer className="pt-20 xl:pt-14 md:pt-12 sm:pt-10 safe-paddings z-50 shrink-0 container">
     <div className="grid grid-cols-12 gap-x-10 2xl:gap-x-9 xl:gap-x-6 lg:gap-x-5 md:grid-cols-4 md:gap-x-4 md:gap-y-10">
       {MENUS.footer.map(({ name, items }, idx) => (
-        <div className="col-span-3 md:col-span-2" key={idx}>
+        <div
+          className="col-span-3 lg:last:col-start-11 lg:last:col-span-2 lg:last:justify-self-center lg:[&:nth-child(3)]:justify-self-center md:col-span-2 md:!justify-self-start md:last:col-start-3"
+          key={idx}
+        >
           <h3 className="text-14 font-bold leading-none tracking-wide text-gray-60">{name}</h3>
-          <ul className="mt-6 flex flex-col gap-2 sm:mt-5">
-            {items.map(({ name: childName, linkUrl }, childIdx) => (
-              <li key={childIdx}>
+          <ul className="mt-7 flex flex-col gap-6">
+            {items.map(({ name: childName, linkUrl, isExternal, withGithubIcon }, childIdx) => (
+              <li className="leading-none" key={childIdx}>
                 <Link
-                  additionalClassName="font-light !leading-tight tracking-wider"
+                  additionalClassName="group font-medium tracking-tight flex items-center"
                   size="md"
                   theme="gray"
                   to={linkUrl}
                 >
-                  {childName}
+                  <span>{childName}</span>
+                  {withGithubIcon && (
+                    <GithubIcon className="ml-1.5 shrink-0 fill-gray-15 group-hover:fill-primary-1 h-5 w-5" />
+                  )}
+                  {isExternal && (
+                    <ExternalIcon className="ml-3 shrink-0 stroke-gray-15 group-hover:stroke-primary-1 h-4 w-4" />
+                  )}
                 </Link>
               </li>
             ))}
@@ -46,7 +57,7 @@ const Footer = () => (
         </div>
       ))}
     </div>
-    <div className="mt-20 grid grid-cols-12 gap-x-10 2xl:gap-x-9 xl:gap-x-6 lg:gap-x-5 sm:gap-x-0 py-6 border-t-4 border-tones-purple-light lg:grid-rows-2 lg:gap-y-6 lg:py-5 sm:grid-rows-4">
+    <div className="mt-20 xl:mt-16 grid grid-cols-12 gap-x-10 2xl:gap-x-9 xl:gap-x-6 lg:gap-x-5 sm:gap-x-0 py-6 border-t-4 border-tones-purple-light lg:grid-rows-2 lg:gap-y-6 lg:py-5 sm:grid-rows-4 lg:mt-[50px] sm:mt-[42px]">
       <div className="col-span-4 2xl:col-span-5 xl:col-span-6 flex items-center gap-x-9 lg:flex-col lg:gap-x-0 lg:gap-y-6 lg:items-start lg:justify-center lg:row-span-2 sm:row-span-1 sm:col-span-full">
         <Link additionalClassName="shrink-0" to="/">
           <span className="sr-only">Bytebase Logo</span>
@@ -75,7 +86,7 @@ const Footer = () => (
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src={icon}
-              className="w-6 h-6 shrink-0 transition-opacity hover:opacity-80"
+              className="w-6 h-6 shrink-0 transition-opacity duration-200 hover:opacity-80"
               alt={name}
             />
           </Link>
@@ -84,14 +95,14 @@ const Footer = () => (
 
       <div className="col-start-10 xl:col-start-11 flex items-center gap-x-8 xl:gap-x-6 lg:col-start-10 lg:row-start-2 lg:col-span-3 lg:justify-self-end sm:row-start-3 sm:justify-self-start sm:col-start-1">
         <Link
-          additionalClassName="text-16 font-medium leading-none tracking-tight"
+          additionalClassName="text-16 sm:text-14 font-medium leading-none tracking-tight"
           theme="gray"
           to={ROUTE.TERMS}
         >
           Terms
         </Link>
         <Link
-          additionalClassName="text-16 font-medium leading-none tracking-tight"
+          additionalClassName="text-16 sm:text-14 font-medium leading-none tracking-tight"
           theme="gray"
           to={ROUTE.PRIVACY}
         >
