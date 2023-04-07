fc5557a6422ee4d7640ff7a30a212c46b5d612e0
Eugen Volodko <chocovelod@gmail.com>
2023-04-07 13:10:30 +0300

    feat: testimonials adaptive

--- src/components/pages/home/testimonials/testimonials.tsx
+++ src/components/pages/home/testimonials/testimonials.tsx
@@ -2,17 +2,17 @@ import QuoteIcon from '@/svgs/quote.inline.svg';
 
 const Testimonials = () => {
   return (
-    <section className="testimonials pt-[176px] pb-[234px] bg-cite">
+    <section className="testimonials pt-[176px] 2xl:pt-36 xl:pt-28 lg:pt-20 pb-[234px] 2xl:pb-[202px] xl:pb-[218px] lg:pb-[154px] md:pb-[130px] bg-cite">
       <div className="container">
-        <figure className="flex flex-col items-center mx-auto max-w-[1048px] text-center">
-          <QuoteIcon className="h-10" />
+        <figure className="flex flex-col items-center mx-auto max-w-[1048px] xl:max-w-[776px] lg:max-w-[590px] text-center">
+          <QuoteIcon className="h-10 xl:h-8 lg:h-7 md:h-6" />
           <blockquote>
-            <p className=" mt-[72px] text-40 leading-[125%] tracking-tight font-semibold">
+            <p className="mt-16 2xl:mt-14 xl:mt-12 lg:mt-10 md:mt-6 text-40 xl:text-30 lg:text-24 md:text-20 leading-[125%] tracking-tight font-semibold">
               Bytebase helped us save $24M in OpEx. We’re also saving 50% of our time with automated
               renewal tracking and alerting across IT, Procurement, and Finance departments
             </p>
           </blockquote>
-          <figcaption className="mt-8 font-semibold tracking-tight leading-none">
+          <figcaption className="mt-8 xl:mt-6 lg:mt-5 md:mt-4 md:text-12 font-semibold tracking-tight leading-none">
             — Brian Crosson, <cite className="font-normal not-italic">HD Supply</cite>
           </figcaption>
         </figure>
