8133bae56bbcb5f8aa768a2e8f2d8a6c04490efd
Eugen Volodko <chocovelod@gmail.com>
2023-04-07 16:54:36 +0300

    fix: home page all components returned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
+import { Change } from '@/components/pages/home/change';
+import { Databases } from '@/components/pages/home/databases';
 import { Features } from '@/components/pages/home/features/features';
+import { Hero } from '@/components/pages/home/hero';
+import { Logos } from '@/components/pages/home/logos';
+import { Options } from '@/components/pages/home/options';
+import { Query } from '@/components/pages/home/query';
+import { Secure } from '@/components/pages/home/secure';
 import { Testimonials } from '@/components/pages/home/testimonials';
 import { Community } from '@/components/shared/community';
+import { SubscriptionForm } from '@/components/shared/subscription-form';
 
 export default function Page() {
   return (
     <>
-      {/* <Hero />
+      <Hero />
       <Databases />
       <Change />
       <Options />
       <Query />
-      <Secure /> */}
+      <Secure />
       <Features />
-      {/* <Logos /> */}
+      <Logos />
       <Testimonials />
       <Community />
-      {/* <SubscriptionForm /> */}
+      <SubscriptionForm />
     </>
   );
 }
