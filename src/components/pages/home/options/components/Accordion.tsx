694345d6362cf22c010d20ec95dd2062fa462e67
Eugen Volodko <chocovelod@gmail.com>
2023-04-10 10:17:49 +0300

    feat: accordion home page draft #1

---
+++ src/components/pages/home/options/components/Accordion.tsx
@@ -0,0 +1,108 @@
+import Image from 'next/image';
+
+import { FC, useState } from 'react';
+
+import clsx from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import AccordionIconClosed from '@/svgs/accordion-arrow-closed.inline.svg';
+import AccordionIcon from '@/svgs/accordion-arrow.inline.svg';
+
+interface AccordionProps {
+  title: string;
+  description: string;
+  image: string;
+  index: number;
+}
+
+const Accordion: FC<AccordionProps> = ({ title, description, image, index }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const isOpen = activeIndex === index;
+
+  const handleToggle = () => {
+    if (isOpen) {
+      setActiveIndex(-1);
+    } else {
+      setActiveIndex(index);
+    }
+  };
+
+  const backgroundColor = `bg-cite bg-tones-purple-light border-t-[1px] border-b-[1px] border-white ${
+    isOpen ? 'bg-opacity-50' : ''
+  }`;
+
+  const titleText = `ml-7 text-30 font-bold leading-[112.5%] -tracking-[0.04em] ${
+    isOpen ? '!ml-0 text-44 font-bold leading-[112.5%] -tracking-[0.04em]' : ''
+  }`;
+
+  return (
+    <div className={backgroundColor}>
+      <div
+        className="cursor-pointer container grid grid-cols-12 gap-x-10 items-center py-[26px]  "
+        onClick={handleToggle}
+      >
+        {isOpen ? (
+          <div className={clsx('flex col-start-1 col-end-8')}>
+            <AccordionIcon className="h-14 shadow-icon rounded-[100%] shrink-0 " />
+            <div className="ml-[30px]">
+              <p className={titleText}>{title}</p>
+              <AnimatePresence>
+                {isOpen && (
+                  <motion.div
+                    className="order-1 flex-grow col-start-1 col-end-8"
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: 'auto' }}
+                    exit={{ opacity: 0, height: 0 }}
+                  >
+                    <p className="mt-4.5 text-18 leading-none">{description}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center col-start-1 col-end-8">
+            <AccordionIconClosed className="h-11 rounded-[100%] shrink-0" />
+            <div className={titleText}>
+              {title}
+              <AnimatePresence>
+                {isOpen && (
+                  <motion.div
+                    className="order-1 flex-grow col-start-1 col-end-8"
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: 'auto' }}
+                    exit={{ opacity: 0, height: 0 }}
+                  >
+                    <p className="mt-4.5 text-18 leading-none">{description}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
+        )}
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="col-start-8 col-end-13"
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+            >
+              <div>
+                <Image
+                  className="order-2 flex-shrink-0 translate-x-3"
+                  src={image}
+                  width={590}
+                  height={320}
+                  alt=""
+                />
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/components/pages/home/options/options.tsx
+++ src/components/pages/home/options/options.tsx
@@ -1,72 +1,53 @@
-import Image from 'next/image';
+'use client';
 
-import AccordionIconClosed from '@/svgs/accordion-arrow-closed.inline.svg';
-import AccordionIcon from '@/svgs/accordion-arrow.inline.svg';
+import Accordion from './components/Accordion';
 
 const Options = () => {
+  const options = [
+    {
+      title: 'Auto style and anti-patterns checks',
+      description:
+        '100+ lint rules to detect SQL anti-patterns and enforce consistent SQL style in the organization.',
+      image: '/images/auto-style.png',
+      index: 1,
+    },
+    {
+      title: 'Batch schema changes to tenant databases',
+      description:
+        '100+ lint rules to detect SQL anti-patterns and enforce consistent SQL style in the organization.',
+      image: '/images/batch-schemas.png',
+      index: 2,
+    },
+    {
+      title: 'Online schema change',
+      description:
+        '100+ lint rules to detect SQL anti-patterns and enforce consistent SQL style in the organization.',
+      image: '/images/online-schemas.png',
+      index: 3,
+    },
+    {
+      title: 'GitOps',
+      description:
+        '100+ lint rules to detect SQL anti-patterns and enforce consistent SQL style in the organization.',
+      image: '/images/git-ops.png',
+      index: 3,
+    },
+  ];
+
   return (
     <section className="options">
-      <h2 className="sr-only">Features</h2>
-      <div className="bg-cite">
-        <div className="container flex items-center pt-16 pb-[52px]">
-          <Image
-            className="order-2 flex-shrink-0 translate-x-3"
-            src="/images/auto-style.png"
-            width={590}
-            height={320}
-            alt=""
-          />
-          <div className="flex">
-            <AccordionIcon className="h-14 shadow-icon rounded-[100%] shrink-0 " />
-            <div className="ml-[30px] order-1 flex-grow">
-              <p>
-                <b className="text-44 font-bold leading-[112.5%] -tracking-[0.04em]">
-                  <mark className="bg-transparent text-primary-1">Auto style</mark> and
-                  anti-patterns checks
-                </b>
-              </p>
-              <p className="mt-4.5 text-18 leading-none">
-                100+ lint rules to detect SQL anti-patterns and enforce consistent SQL style in the
-                organization.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-px bg-tones-purple-light">
-        <div className="container">
-          <div className="py-8">
-            <div className="flex items-center">
-              <AccordionIconClosed className="h-11 rounded-[100%] shrink-0" />
-              <b className="ml-7 text-30 font-bold leading-[112.5%] -tracking-[0.04em]">
-                Batch schema changes to tenant databases
-              </b>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-px bg-tones-purple-light">
-        <div className="container">
-          <div className="flex items-center">
-            <AccordionIconClosed className="h-11 rounded-[100%] shrink-0" />
-            <p className="py-8">
-              <b className="ml-7 text-30 font-bold leading-[112.5%] -tracking-[0.04em]">
-                Online schema change
-              </b>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="mt-px bg-tones-purple-light">
-        <div className="container">
-          <div className="flex items-center">
-            <AccordionIconClosed className="h-11 rounded-[100%] shrink-0" />
-            <p className="py-8">
-              <b className="ml-7 text-30 font-bold leading-[112.5%] -tracking-[0.04em]">GitOps</b>
-            </p>
-          </div>
-        </div>
-      </div>
+      <ul>
+        {options.map((option, index) => (
+          <li key={index}>
+            <Accordion
+              title={option.title}
+              description={option.description}
+              image={option.image}
+              index={index}
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
