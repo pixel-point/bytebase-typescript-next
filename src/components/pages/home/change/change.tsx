b7a92ee3082c6150f2bddf3b2d7e0dc7e9c324d2
Eugen Volodko <chocovelod@gmail.com>
2023-04-10 13:26:28 +0300

    feat: new images on changes component

--- src/components/pages/home/change/change.tsx
+++ src/components/pages/home/change/change.tsx
@@ -1,34 +1,67 @@
 import Image from 'next/image';
-import NextLink from 'next/link';
+
+import { LinkUnderlined } from '@/components/shared/link-underlined';
 
 const Change = () => {
   return (
-    <section className="change mt-[160px] bg-black text-white">
-      <div className="container grid grid-cols-12 grid-gap">
-        <header className="col-span-4 self-end">
+    <section className="change mt-[160px] 2xl:mt-36 xl:mt-32 lg:mt-24 md:mt-20 bg-black text-white lg:pt-16">
+      <div className="container grid grid-cols-12 gap-x-10 2xl:gap-x-9 xl:gap-x-6 lg:gap-x-5">
+        <div className="self-center col-start-1 col-end-7 xl:col-end-6 lg:col-end-13">
           <span className="inline-flex font-bold uppercase text-12 leading-none tracking-[0.02em] bg-primary-1 px-2.5 py-2 rounded-3xl">
             Change
           </span>
-          <h2 className="font-title font-semibold leading-none text-112">
-            <mark className="bg-transparent text-secondary-1">Automate</mark> database changes
+          <h2 className=" mt-[14px] font-title font-semibold leading-none text-112 xl:text-90 lg:text-80 md:text-48">
+            <span className="bg-transparent text-secondary-1 ">Automate</span> database changes
           </h2>
-          <p className="mt-6 text-20 leading-[150%]">
+          <p className="mt-6 xl:mt-4 lg:mt-2 text-20 xl:text-18 lg:text-16 leading-[150%] max-w-[504px] 2xl:max-w-[460px] lg:max-w-[590px]">
             Bytebase streamlines database deployment from non-prod to prod by integrating with
             version control systems for a GitOps workflow.
           </p>
-        </header>
-        <div className="row-span-2 col-span-8">
+          <div className="mt-8 md:mt-3 col-span-4 self-start">
+            <LinkUnderlined to="">Learn more</LinkUnderlined>
+          </div>
+        </div>
+        <div className="col-start-7 2xl:col-start-7 xl:col-start-6 col-end-13">
           <Image
-            className="ml-auto mr-[110px]"
-            src="/images/automate-database-changes.png"
-            width={620}
+            className="ml-auto 2xl:hidden"
+            src="/images/automate-database-changes-xxl.png"
+            width={716}
             height={904}
             alt=""
           />
+          <Image
+            className="ml-auto hidden 2xl:block xl:hidden"
+            src="/images/automate-database-changes-xl.png"
+            width={654}
+            height={874}
+            alt=""
+          />
+          <Image
+            className="ml-auto hidden xl:block lg:hidden"
+            src="/images/automate-database-changes-lg.png"
+            width={536}
+            height={743}
+            alt=""
+          />
         </div>
-        <footer className="mt-8 col-span-4 self-start">
-          <NextLink href="#">Learn more</NextLink>
-        </footer>
+      </div>
+      <div className=" mt-10 hidden lg:block relative w-full min-h-[654px] md:min-h-[436px]">
+        <Image
+          className="ml-auto  hidden lg:block sm:hidden h-[654px]"
+          src="/images/automate-database-changes-md.png"
+          alt=""
+          objectPosition="top"
+          objectFit="cover"
+          fill
+        />
+        <Image
+          className="ml-auto hidden sm:block"
+          src="/images/automate-database-changes-sm.png"
+          alt=""
+          objectPosition="top"
+          objectFit="cover"
+          fill
+        />
       </div>
     </section>
   );
--- src/components/pages/home/databases/databases.tsx
+++ src/components/pages/home/databases/databases.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const Databases = () => {
   return (
-    <section className="databases mt-[248px] container grid grid-cols-12 grid-gap">
-      <h2 className="col-span-10 col-start-2 font-title font-semibold leading-none text-88 lg:text-68 text-center">
+    <section className="databases mt-[248px] 2xl:mt-[160px] xl:mt-[128px] md:mt-[96px] sm:mt-[80px] container grid grid-cols-12 grid-gap">
+      <h2 className="col-span-10 col-start-2 lg:col-start-1 lg:col-end-13 font-title font-semibold leading-none text-88 xl:text-68 lg:text-54 md:text-48 text-center">
         Change, Query, Secure, Govern{' '}
         <mark className="bg-transparent text-primary-1 whitespace-nowrap text-center">
           all databases
@@ -11,7 +11,14 @@ const Databases = () => {
         in a single place
       </h2>
       <div className="mt-[68px] col-span-12">
-        <Image src="/images/scheme.png" width={1472} height={600} alt="" />
+        <Image src="/images/scheme.png" width={1472} height={650} alt="" className="sm:hidden" />
+        <Image
+          src="/images/scheme-mobile.png"
+          width={1472}
+          height={650}
+          alt=""
+          className="hidden sm:block"
+        />
       </div>
     </section>
   );
