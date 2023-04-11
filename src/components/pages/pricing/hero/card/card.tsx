c11a47c94e52fe7b42dc11a94b5207be86d0900e
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-04-11 19:26:07 +0400

    feat: add hero for 768px

--- src/components/pages/pricing/hero/card/card.tsx
+++ src/components/pages/pricing/hero/card/card.tsx
@@ -34,31 +34,37 @@ const Card = ({
   const planColor = colors[planTitle.toLowerCase() as keyof typeof colors];
   return (
     <article
-      className="bg-white h-full border border-gray-70 shadow-pricing flex flex-col items-center border-t-8 pt-3.5 pb-10 px-6 lg:px-5"
+      className="bg-white h-full border border-gray-70 shadow-pricing flex flex-col items-center border-t-8 pt-3.5 md:pt-2.5 pb-10 px-6 lg:px-5 md:px-4.5 md:pb-9"
       style={{ borderTopColor: planColor }}
     >
-      <div className="flex flex-col items-center min-h-[548px] lg:min-h-[525px] w-full">
+      <div className="flex flex-col items-center min-h-[548px] lg:min-h-[525px] w-full md:min-h-[462px]">
         <h2
           className="self-start tracking-wide font-bold uppercase text-16 leading-none"
           style={{ color: planColor }}
         >
           {planTitle}
         </h2>
-        <Image src={image} width={240} height={240} alt={planTitle} className="mt-3 lg:w-[220px]" />
+        <Image
+          src={image}
+          width={240}
+          height={240}
+          alt={planTitle}
+          className="mt-3 lg:w-[220px] md:w-[180px]"
+        />
         <span
           className={clsx(
-            'mt-4 relative font-title font-semibold text-104 leading-none text-gray-15 tracking-tighter lg:text-72 lg:tracking-normal',
+            'mt-4 relative font-title font-semibold text-104 leading-none text-gray-15 tracking-tighter lg:text-72 lg:tracking-normal md:text-56 md:mt-3',
             { 'text-primary-1': planTitle.toLowerCase() === 'team' },
           )}
         >
           {typeof price === 'number' ? `$${price}` : price}
           {priceDescription && (
-            <span className="tracking-normal text-gray-15 font-sans text-15 leading-none font-normal absolute -right-[76px] top-4 max-w-[64px] lg:-right-[72px]">
+            <span className="tracking-normal text-gray-15 font-sans text-15 leading-none font-normal absolute -right-[76px] top-4 max-w-[64px] lg:-right-[72px] md:text-14 md:top-2">
               {priceDescription}
             </span>
           )}
         </span>
-        <p className="mt-2 lg:mt-3.5 text-gray-30 text-16 leading-normal lg:leading-snug text-center lg:max-w-[180px]">
+        <p className="mt-2 lg:mt-3.5 text-gray-30 text-16 leading-normal lg:leading-snug text-center lg:max-w-[180px] md:mt-1 md:text-14">
           {description}
         </p>
         <Button additionalClassName="w-full mt-5" size="lg" theme={buttonTheme} to={buttonLink}>
@@ -66,18 +72,18 @@ const Card = ({
         </Button>
         {additionalDescription && (
           <p
-            className="mt-3 with-medium-text text-14 leading-tight tracking-tight text-gray-40"
+            className="mt-3 with-medium-text text-14 leading-tight tracking-tight text-gray-40 md:leading-snug text-center md:mt-4 md:max-w-[150px]"
             dangerouslySetInnerHTML={{ __html: additionalDescription }}
           />
         )}
       </div>
-      <footer className="border-t border-gray-70 pt-6 lg:pt-[26px] w-full">
-        <ul className="flex flex-col gap-y-4 lg:gap-x-4.5">
+      <footer className="border-t border-gray-70 pt-6 w-full md:pt-5">
+        <ul className="flex flex-col gap-y-4">
           {planConditions.map((condition, index) => (
             <li className="flex items-center lg:items-start gap-x-2" key={index}>
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img className="w-5 h-5 shrink-0" src="/images/check.svg" alt="" />
-              <span className="text-15 lg:leading-normal leading-none text-gray-15">
+              <span className="text-15 lg:leading-normal leading-none text-gray-15 md:text-14 md:leading-snug">
                 {condition}
               </span>
             </li>
--- src/components/pages/pricing/hero/hero.tsx
+++ src/components/pages/pricing/hero/hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       price: 19,
       priceDescription: '/ starts at month',
       description: 'Up to 10 seats and 10 instances (first 3 FREE)',
-      additionalDescription: 'You can start <b>a free trial for 14 days.</b>',
+      additionalDescription: 'You can start <b>a&nbsp;free&nbsp;trial for 14 days.</b>',
       buttonText: 'Try Free now',
       buttonTheme: 'primary-filled',
       buttonLink:
@@ -78,26 +78,26 @@ const Hero = () => {
   ];
 
   return (
-    <section className="hero pt-[120px] 2xl:pt-[108px] bg-pricing-hero pb-20 lg:pb-16">
+    <section className="hero pt-[120px] 2xl:pt-[108px] bg-pricing-hero pb-20 lg:pb-16 md:pt-24 md:pb-[54px]">
       <div className="container max-w-[1396px] 2xl:max-w-full">
         <header className="text-center">
           <h1 className="sr-only">Bytebase pricing page</h1>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/images/pricing/quote.svg" alt="" className="mx-auto" />
-          <p className="mt-4 font-semibold leading-tight text-40 tracking-tighter max-w-[1048px] mx-auto lg:max-w-[800px] lg:text-30 lg:leading-extra-tight">
+          <img src="/images/pricing/quote.svg" alt="" className="mx-auto md:w-10 md:h-auto" />
+          <p className="mt-4 font-semibold leading-tight text-40 tracking-tighter max-w-[1048px] mx-auto lg:max-w-[800px] lg:text-30 lg:leading-extra-tight md:mt-6 md:text-24 md:tracking-tight md:max-w-[670px]">
             Bytebase is not a better tool to manage database changes. It&apos;s a better way to
             manage database changes.
           </p>
-          <p className="text-gray-40 mt-3.5 text-18 leading-extra-tight lg:text-16 lg:leading-snug">
+          <p className="text-gray-40 mt-3.5 text-18 leading-extra-tight lg:text-16 lg:leading-snug md:mt-4.5 md:text-14">
             Built by engineers knowing database development flow
           </p>
-          <ul className="flex items-center gap-x-16 mt-11 lg:gap-x-[52px] justify-center">
+          <ul className="flex items-center gap-x-16 mt-11 lg:gap-x-[52px] justify-center md:mt-10 md:gap-x-10">
             <li>
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
                 src="/images/pricing/microsoft.svg"
                 alt="Microsoft logo"
-                className="h-10 w-auto lg:h-9"
+                className="h-10 w-auto lg:h-9 md:h-[30px]"
               />
             </li>
             <li>
@@ -105,7 +105,7 @@ const Hero = () => {
               <img
                 src="/images/pricing/google.svg"
                 alt="Google logo"
-                className="h-10 w-auto lg:h-9"
+                className="h-10 w-auto lg:h-9 md:h-[30px]"
               />
             </li>
             <li>
@@ -113,7 +113,7 @@ const Hero = () => {
               <img
                 src="/images/pricing/ant-group.svg"
                 alt="Ant Group logo"
-                className="h-10 w-auto lg:h-9"
+                className="h-10 w-auto lg:h-9 md:h-[30px]"
               />
             </li>
             <li>
@@ -121,19 +121,19 @@ const Hero = () => {
               <img
                 src="/images/pricing/pingcap.svg"
                 alt="Pingcap logo"
-                className="h-10 w-auto lg:h-9"
+                className="h-10 w-auto lg:h-9 md:h-[30px]"
               />
             </li>
           </ul>
         </header>
-        <ul className="mt-14 lg:mt-12 flex gap-x-8 lg:gap-x-6">
+        <ul className="mt-14 lg:mt-12 flex gap-x-8 lg:gap-x-6 md:gap-x-5">
           {cards.map((card, idx) => (
             <li className="grow basis-1/3" key={idx}>
               <Card {...card} />
             </li>
           ))}
         </ul>
-        <p className="mt-9 text-15 leading-snug text-center text-gray-40 lg:mt-8 lg:text-14 lg:leading-tight">
+        <p className="mt-9 text-15 leading-snug text-center text-gray-40 lg:mt-8 lg:text-14 lg:leading-tight md:mt-7 sm:mt-6">
           You can upgrade, downgrade, or{' '}
           <Link size="sm" to="/refund" theme="primary-1" additionalClassName="lg:text-14">
             cancel your subscription
