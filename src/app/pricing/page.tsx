13c97e032b9ed6d9669954c8c53327a92013d989
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2023-04-14 18:57:33 +0400

    chore: add shared components

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,11 +1,15 @@
 import Hero from '@/components/pages/pricing/hero';
 import Table from '@/components/pages/pricing/table';
+import Community from '@/components/shared/community';
+import SubscriptionForm from '@/components/shared/subscription-form';
 
 export default function Page() {
   return (
     <>
       <Hero />
       <Table />
+      <Community />
+      <SubscriptionForm />
     </>
   );
 }
