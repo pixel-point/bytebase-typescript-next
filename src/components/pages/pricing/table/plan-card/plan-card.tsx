14fce1a2c78350454242d91a48864e677664a2bb
Sasha Sushko <sasha@sushko.me>
2023-04-15 09:48:35 +0300

    fix: pixelperfect in table

--- src/components/pages/pricing/table/plan-card/plan-card.tsx
+++ src/components/pages/pricing/table/plan-card/plan-card.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 
-import { COLORS } from '@/components/pages/pricing/table/data/pricing-plans';
 import Button from '@/components/shared/button';
 
 import { Plan } from '@/types/pricing';
@@ -12,44 +11,37 @@ const PlanCard = ({
   buttonText,
   buttonUrl,
   buttonTheme,
-  className,
-}: Plan & {
-  className: string;
-}) => {
-  const planColor = COLORS[title];
-
+}: Plan) => {
   return (
     <div
       className={clsx(
-        'relative flex flex-col text-center border border-tones-purple-dark md:min-w-[284px] 2xs:min-w-fit 2xs:w-[156px]',
-        className,
-        { 'bg-[#F9FAFF] border-l-0 border-r-0': title === 'team' },
+        'h-[257px] border border-tones-purple-dark text-center before:mb-6 before:block before:h-2 before:w-full 3xl:h-[272px] xl:h-[254px] md:h-[245px] sm:h-[243px] sm:before:mb-5',
+        title === 'team' ? 'border-l-0 border-r-0 bg-[#F9FAFF]' : 'bg-white',
+        {
+          'before:bg-[#5647EB]': title === 'team',
+          'before:bg-[#3DB8F5]': title === 'free',
+          'before:bg-[#172136]': title === 'enterprise',
+        },
       )}
     >
-      <div
-        className={clsx(
-          'border-t-8 flex flex-col px-4.5 2xl:px-6 pt-6 lg:px-4.5 min-h-[250px] lg:min-h-[230px] z-20 xs:px-2 sm:pt-5',
-          title === 'team' ? 'bg-[#F9FAFF]' : 'bg-white',
-        )}
-        style={{ borderTopColor: planColor }}
-      >
-        <h3 className="font-title capitalize text-56 leading-none lg:text-44 md:text-34">
+      <div className="flex flex-col px-4 sm:px-2">
+        <h3 className="font-title text-56 capitalize leading-none xl:text-44 md:text-34">
           {title}
         </h3>
         <p
-          className="mt-3 text-14 leading-tight tracking-tight text-gray-40 max-w-[244px] mx-auto lg:mt-2 2xl:min-h-[54px] sm:min-h-[72px]"
+          className="mx-auto mt-3 max-w-[244px] text-14 leading-tight tracking-tight text-gray-40 3xl:min-h-[54px] xl:mt-2 sm:min-h-[72px]"
           dangerouslySetInnerHTML={{ __html: description }}
         />
         <Button
           theme={buttonTheme}
           size="md"
           to={buttonUrl}
-          additionalClassName="mt-5 w-[232px] xl:w-full mx-auto lg:mt-4 md:mt-5"
+          additionalClassName="mt-5 w-[232px] 3xl:w-full mx-auto xl:mt-4 md:mt-5 sm:mt-3"
         >
           {buttonText}
         </Button>
         {additionalDescription && (
-          <p className="mt-4 text-gray-15 text-14 leading-tight tracking-tight sm:mt-2.5">
+          <p className="mt-4 text-14 leading-tight tracking-tight text-gray-15 sm:mt-2.5">
             {additionalDescription}
           </p>
         )}
@@ -57,8 +49,8 @@ const PlanCard = ({
           className={clsx(
             'w-full',
             additionalDescription
-              ? 'mt-5 lg:mt-4 sm:mt-2.5'
-              : 'mt-[54px] lg:mt-[50px] sm:mt-[38px]',
+              ? 'mt-5 xl:mt-4 sm:mt-2.5'
+              : 'mt-[54px] xl:mt-[50px] sm:mt-[38px]',
           )}
         />
       </div>
--- src/components/pages/pricing/table/table.tsx
+++ src/components/pages/pricing/table/table.tsx
@@ -36,104 +36,105 @@ const Table = () => {
   }, []);
 
   return (
-    <section className="pt-20 xl:pt-16 md:pt-14 sm:pt-10">
-      <div className="mx-auto max-w-[1220px] 2xl:px-12 xl:px-11 md:px-7 sm:px-4 2xl:max-w-full relative">
-        <Image
-          src="/images/pricing/table-decor.png"
-          width={338}
-          height={330}
-          alt=""
-          className="absolute -top-[140px] -left-10 2xl:left-24 z-50 lg:w-[280px] lg:h-auto md:w-[225px] lg:-top-[100px] lg:left-11 md:-top-3 md:left-7 sm:w-[204px] sm:-left-14 sm:top-0"
-        />
-        <div className="flex items-start relative">
-          <div className="min-w-[378px] grow 2xl:min-w-[460px] lg:min-w-[321px] md:min-w-[244px] relative flex flex-col z-40 sm:min-w-[156px] shadow-labels">
-            <div className="z-40 bg-white h-[219px] 2xl:h-[241px] w-full lg:h-[216px] md:h-[210px]" />
-            <div className="bg-white z-40">
-              {LABELS.map(({ title, items }, idx) => (
-                <div
-                  className="relative mt-11 border-b border-black border-opacity-10 first:mt-4 md:first:mt-5 sm:first:mt-4 last:border-b-0"
-                  key={`${title}_${idx}`}
-                >
-                  <p className="text-24 font-bold leading-none lg:text-20 lg:leading-tight sm:text-18">
-                    {title}
-                  </p>
-                  <ul className="mt-4 flex flex-col divide-y divide-black divide-opacity-10">
-                    {Object.keys(items).map((item, rowIdx) => {
-                      const isActive = `${item}-${rowIdx}` === currentRow;
-                      return (
-                        <li
-                          className={clsx(
-                            'flex h-12 items-center text-16 font-medium leading-normal text-gray-15 sm:text-15 sm:pr-4.5',
-                            {
-                              'bg-[#FCFBFF]': isActive,
-                            },
+    <>
+      <section className="container grid-gap grid grid-cols-12 pt-20 xl:pt-16 md:pt-14 sm:pt-3.5">
+        <div className="relative z-20 col-span-3 col-start-2 3xl:col-span-4 3xl:col-start-1 sm:col-span-6">
+          <Image
+            src="/images/pricing/table-decor.png"
+            width={338}
+            height={330}
+            alt=""
+            className="absolute -top-[140px] -left-10 max-w-none 3xl:left-0 3xl:-top-[124px] 3xl:h-auto 3xl:w-[345px] xl:-top-[100px] xl:w-[280px] md:-top-[10px] md:w-[225px] sm:-top-px sm:-left-[68px] sm:w-[204px]"
+          />
+          <div className="-mr-10 flex grow flex-col pt-[233px] shadow-labels 3xl:-mr-9 3xl:pt-[251px] xl:-mr-6 xl:pt-[232px] md:-mr-5 md:pt-[227px] sm:mr-0 sm:pt-[219px]">
+            {LABELS.map(({ title, items }, idx) => (
+              <div
+                className="relative mt-11 border-b border-black border-opacity-10 first:mt-0 last:border-b-0"
+                key={`${title}_${idx}`}
+              >
+                <p className="text-24 font-bold leading-none lg:text-20 lg:leading-tight sm:text-18">
+                  {title}
+                </p>
+                <ul className="mt-4 flex flex-col divide-y divide-black divide-opacity-10">
+                  {Object.keys(items).map((item, rowIdx) => {
+                    const isActive = `${item}-${rowIdx}` === currentRow;
+                    return (
+                      <li
+                        className={clsx(
+                          'flex h-12 items-center text-16 font-medium leading-normal text-gray-15 sm:pr-4.5 sm:text-15',
+                          {
+                            'bg-[#FCFBFF]': isActive,
+                          },
 
-                            calculateCellHeight(item),
-                          )}
-                          data-row-id={`${item}-${rowIdx}`}
-                          key={`${item}_${rowIdx}`}
-                        >
-                          <span className="lg:max-w-[300px] md:max-w-[220px]">
-                            {items[item as keyof typeof items]}
-                          </span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex md:overflow-x-auto relative md:scrollbar-hidden max-w-[1220px] 2xl:max-w-full md:max-w-none mx-auto md:h-full md:-mr-7 md:pr-7 sm:-mr-4 sm:pr-4">
-            {Object.keys(PLANS).map((plan, idx) => {
-              const currentPlan = PLANS[plan as keyof typeof PLANS];
-
-              return (
-                <div className="flex flex-col" key={`${currentPlan.title}_${idx}`}>
-                  <div className="z-20 sticky top-0">
-                    <PlanCard className="basis-1/3 grow" {...currentPlan} />
-                  </div>
-                  <div
-                    className={clsx(
-                      'basis-1/3 grow border border-t-0 border-tones-purple-dark md:min-w-[284px] 2xs:min-w-fit 2xs:w-[156px]',
-                      { 'border-l-0 border-r-0 bg-[#F9FAFF]': currentPlan.title === 'team' },
-                    )}
-                  >
-                    <FeatureList
-                      title={currentPlan.title}
-                      features={currentPlan.changeManagement}
-                      currentRow={currentRow}
-                    />
-                    <FeatureList
-                      title={currentPlan.title}
-                      features={currentPlan.sql}
-                      currentRow={currentRow}
-                    />
-                    <FeatureList
-                      title={currentPlan.title}
-                      features={currentPlan.collaboration}
-                      currentRow={currentRow}
-                    />
-                    <FeatureList
-                      title={currentPlan.title}
-                      features={currentPlan.security}
-                      currentRow={currentRow}
-                      withLongTitle
-                    />
-                    <FeatureList
-                      title={currentPlan.title}
-                      features={currentPlan.bespoke}
-                      currentRow={currentRow}
-                      isLastSection
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                          calculateCellHeight(item),
+                        )}
+                        data-row-id={`${item}-${rowIdx}`}
+                        key={`${item}_${rowIdx}`}
+                      >
+                        <span className="lg:max-w-[300px] md:max-w-[220px]">
+                          {items[item as keyof typeof items]}
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-    </section>
+        <div className="relative z-10 col-span-7 3xl:col-span-8 sm:col-span-6">
+          <div className="md:scrollbar-hidden md:-mr-7 md:overflow-x-auto sm:-mx-4">
+            <div className="flex flex-row items-stretch justify-start md:w-[852px] sm:w-[468px]">
+              {Object.keys(PLANS).map((plan, idx) => {
+                const currentPlan = PLANS[plan as keyof typeof PLANS];
+                const isMiddleRow = currentPlan.title === 'team';
+
+                return (
+                  <div className="shrink grow sm:w-1/3" key={`${currentPlan.title}_${idx}`}>
+                    <div className="sticky top-0 z-30 md:static">
+                      <PlanCard {...currentPlan} />
+                    </div>
+                    <div
+                      className={clsx('border border-t-0 border-tones-purple-dark', {
+                        'border-l-0 border-r-0 bg-[#F9FAFF]': isMiddleRow,
+                      })}
+                    >
+                      <FeatureList
+                        title={currentPlan.title}
+                        features={currentPlan.changeManagement}
+                        currentRow={currentRow}
+                      />
+                      <FeatureList
+                        title={currentPlan.title}
+                        features={currentPlan.sql}
+                        currentRow={currentRow}
+                      />
+                      <FeatureList
+                        title={currentPlan.title}
+                        features={currentPlan.collaboration}
+                        currentRow={currentRow}
+                      />
+                      <FeatureList
+                        title={currentPlan.title}
+                        features={currentPlan.security}
+                        currentRow={currentRow}
+                        withLongTitle
+                      />
+                      <FeatureList
+                        title={currentPlan.title}
+                        features={currentPlan.bespoke}
+                        currentRow={currentRow}
+                        isLastSection
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
