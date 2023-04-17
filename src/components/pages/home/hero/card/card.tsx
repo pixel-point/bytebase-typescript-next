49ef9406eaac3cea330823a9d86de9d461b0f846
Sasha Sushko <sasha@sushko.me>
2023-04-17 23:06:33 +0300

    feat: fraft: hero animation

--- src/components/pages/home/hero/card/card.tsx
+++ src/components/pages/home/hero/card/card.tsx
@@ -4,49 +4,53 @@ import clsx from 'clsx';
 
 import { LinkUnderlined } from '@/components/shared/link-underlined';
 
-interface CardProps {
+export type CardProps = {
+  color: 'blue' | 'green' | 'red';
   cover: string;
   href: string;
   title: string;
   description: string;
-  image?: string;
-}
+  className?: string;
+};
 
-const Card = ({ cover, title, href, description, image }: CardProps) => {
+const Card = ({ color, className, cover, title, href, description }: CardProps) => {
   return (
-    <article className="relative">
-      {image && (
-        <Image
-          src={image}
-          width={92}
-          height={65}
-          alt=""
-          className="absolute -top-7 -left-9 lg:-top-4.5 lg:-left-6 lg:h-[42px] lg:w-[58px] sm:h-[46px] sm:w-16"
-        />
-      )}
-      <Image
-        className={clsx({
-          'w-full shadow-[0px_5px_15px_0px_rgba(156,186,201,0.5)]':
-            cover === '/images/change-database.png',
-          'w-full shadow-[0px_5px_15px_0px_rgba(143,188,169,0.4)]':
-            cover === '/images/query-data.png',
-          'w-full shadow-[0px_5px_15px_0px_rgba(210,172,210,0.4)]':
-            cover === '/images/secure-access.png',
-        })}
-        src={cover}
-        width={464}
-        height={600}
-        alt=""
-      />
-      <h3 className="mt-8 text-36 font-bold leading-extra-tight tracking-tighter lg:mt-6 lg:text-32 lg:tracking-normal md:mt-5 md:text-30">
-        {title}
-      </h3>
-      <p className="mt-4 max-w-[75%] text-20 leading-normal 2xl:max-w-none lg:mt-3 lg:text-18 lg:leading-snug md:mt-1.5 md:text-16">
-        {description}
-      </p>
-      <LinkUnderlined to={href} className="mt-6 lg:mt-5 md:mt-3">
-        Learn more
-      </LinkUnderlined>
+    <article className={clsx('flip', className)}>
+      <div className="flip-inner grid">
+        <div className="flip-front col-span-full row-span-full">
+          <Image
+            className="h-auto w-full"
+            src="/images/page/main/hero/video.png"
+            width={464}
+            height={600}
+            alt=""
+          />
+        </div>
+        <div className="flip-back col-span-full row-span-full">
+          <Image
+            className={clsx('h-auto w-full', {
+              'shadow-blue': color === 'blue',
+              'shadow-green': color === 'green',
+              'shadow-red': color === 'red',
+            })}
+            src={cover}
+            width={464}
+            height={600}
+            alt=""
+          />
+          <div>
+            <h3 className="mt-8 overflow-hidden text-ellipsis whitespace-nowrap text-36 font-bold leading-extra-tight tracking-tighter xl:mt-6 xl:text-32 md:mt-5 md:text-30">
+              {title}
+            </h3>
+            <p className="mt-4 h-[90px] w-3/4 text-20 leading-normal 3xl:w-full xl:relative xl:mt-3 xl:h-[81px] xl:overflow-hidden xl:text-18 xl:after:absolute xl:after:bottom-0 xl:after:right-0 xl:after:h-[30px] xl:after:w-2/3 xl:after:bg-gradient-to-r xl:after:from-transparent xl:after:to-white md:mt-1.5 md:h-[72px] md:text-16">
+              {description}
+            </p>
+            <LinkUnderlined to={href} className="mt-6 xl:mt-5 md:mt-3">
+              Learn more
+            </LinkUnderlined>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -5,27 +5,29 @@ import Route from '@/lib/route';
 
 import SmallArrowIcon from '@/svgs/small-arrow.inline.svg';
 
-import Card from './card';
+import Card, { type CardProps } from './card';
 
-const cards = [
+const cards: CardProps[] = [
   {
+    color: 'blue',
     href: '#',
-    cover: '/images/change-database.png',
+    cover: '/images/page/main/hero/change-database.png',
     title: 'Change database',
     description:
       'Simplify database changes with standardization, lint rules, and GitOps integration.',
-    image: '/images/hat.png',
   },
   {
+    color: 'green',
     href: '#',
-    cover: '/images/query-data.png',
+    cover: '/images/page/main/hero/query-data.png',
     title: 'Query data',
     description:
       "Perform complex SQL tasks and protect data privacy with ByteBase's web-based IDE.",
   },
   {
+    color: 'red',
     href: '#',
-    cover: '/images/secure-access.png',
+    cover: '/images/page/main/hero/secure-access.png',
     title: 'Secure access',
     description:
       'Eliminate administrative complexity and ensure compliance with one-stop resource management.',
@@ -34,65 +36,46 @@ const cards = [
 
 const Hero = () => {
   return (
-    <section className="container gap-x-grid mt-32 grid grid-cols-12 2xl:mt-[120px] lg:mt-[120px] md:mt-[104px] sm:mt-24 sm:grid-cols-4">
-      <div className="col-span-12 row-start-1 sm:col-span-4">
-        <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1">
-          <span className="rounded-full bg-primary-1 px-2 py-1 text-white">What’s new?</span>
-          <span className="flex items-center gap-1.5 px-2">
-            1.10.0 Released
-            <SmallArrowIcon width={7} height={6} />
-          </span>
-        </span>
-      </div>
-      <header className="relative col-span-8 row-start-2 mt-4.5 max-w-[844px] 2xl:col-span-9 2xl:max-w-[824px] lg:mt-3.5 lg:max-w-[696px] md:col-span-12 sm:col-span-4 sm:mt-3">
-        <h1 className="sm:hidden">
-          <span className="font-title text-112 font-semibold leading-none lg:text-90 lg:leading-95 md:text-80 sm:text-48">
-            <mark className="bg-transparent text-primary-1">Database</mark> schema change and
-            version
-          </span>
-          <span className="flex sm:flex-col">
-            <span className="font-title text-112 font-semibold leading-none lg:text-90 lg:leading-95 md:text-80 sm:text-48">
-              control
+    <div className="container gap-x-grid mt-32 grid grid-cols-12 2xl:mt-[120px] lg:mt-[120px] md:mt-[104px] sm:mt-24">
+      <section className="col-start-1 col-end-8 row-start-1 row-end-2 3xl:col-end-9 xl:col-end-10 md:col-end-12">
+        <header>
+          {/* TODO: wrap to a link */}
+          <span className="inline-flex items-center gap-1 rounded-full bg-tones-purple-light p-1 text-12 font-semibold leading-none text-primary-1">
+            <span className="rounded-full bg-primary-1 px-2 py-1 text-white">What’s new?</span>
+            <span className="flex items-center gap-1.5 px-2">
+              1.10.0 Released
+              <SmallArrowIcon width={7} height={6} />
             </span>
-            <p className="ml-11 mt-5 text-20 leading-[140%] 2xl:ml-[42px] xl:text-18 lg:ml-9 lg:mt-[7px] md:ml-5 md:mt-2 md:max-w-sm md:text-16">
+          </span>
+          <div className="relative mt-4 xl:mt-3.5">
+            <h1 className="font-title text-112 font-semibold leading-none xl:text-90 xl:leading-95 md:text-80 sm:text-48">
+              <mark className="bg-transparent text-primary-1">Database</mark> schema change and
+              version control
+            </h1>
+            <p className="absolute top-[244px] left-[338px] max-w-lg text-20 leading-140 xl:top-[177px] xl:left-[271px] xl:text-18 xl:leading-snug md:left-[231px] md:top-[158px] md:text-16">
               Bytebase offers a web-based collaboration workspace to help DBAs and Developers manage
               the lifecycle of application database schemas.
             </p>
-          </span>
-        </h1>
-        <div className="hidden max-w-sm sm:block">
-          <h1 className="font-title text-48 font-semibold leading-95">
-            <mark className="bg-transparent text-primary-1">Database</mark> schema change and
-            version control
-          </h1>
-          <p className="mt-3 text-16 leading-[140%]">
-            Bytebase offers a web-based collaboration workspace to help DBAs and Developers manage
-            the lifecycle of application database schemas.
-          </p>
-        </div>
-      </header>
-
-      <ul className="gap-x-grid col-span-12 row-start-4 mt-20 grid grid-cols-12 sm:col-span-4 sm:mt-7 sm:grid-cols-4 sm:gap-y-8">
-        {cards.map((card, idx) => (
-          <li key={idx} className="col-span-4 sm:col-span-3 2xs:col-span-4">
-            <Card {...card} />
-          </li>
-        ))}
-      </ul>
-      <footer className="col-span-4 row-start-3 mt-14 flex items-center gap-9 lg:col-span-6 lg:mt-[46px] md:mt-[30px] sm:col-span-4 sm:mt-6">
-        <Button
-          to={Route.INDEX}
-          theme="primary-filled"
-          size="lg"
-          additionalClassName="sm:text-13 md:py-[18px] md:px-[36px] md:max-w-[167px] md:text-13 tracking-wide w-full max-w-[218px]"
-        >
-          Request a Demo
-        </Button>
-        <LinkUnderlined className="min-w-fit md:-ml-4" to={Route.INDEX}>
-          See Live Demo
-        </LinkUnderlined>
-      </footer>
-    </section>
+          </div>
+        </header>
+        <footer className="mt-14 flex items-center gap-9 xl:mt-11 md:mt-7">
+          <Button to={Route.INDEX} theme="primary-filled" size="lg">
+            Request a Demo
+          </Button>
+          <LinkUnderlined to={Route.INDEX}>See Live Demo</LinkUnderlined>
+        </footer>
+      </section>
+      <div className="col-span-full col-start-1 row-start-2 h-20 md:h-[60px]" />
+      <div className="col-start-1 col-end-5 row-start-3 row-end-4">
+        <Card className="sticky top-[80px]" {...cards[0]} />
+      </div>
+      <div className="col-start-5 col-end-9 row-start-1 row-end-4 pt-[461px] xl:row-start-2 xl:pt-[30px]">
+        <Card className="sticky top-[80px]" {...cards[1]} />
+      </div>
+      <div className="col-start-9 col-end-13 row-start-1 row-end-4 pt-[136px] xl:row-start-2 xl:-mt-[40px] xl:pt-0 md:-mt-[29px]">
+        <Card className="sticky top-[80px]" {...cards[2]} />
+      </div>
+    </div>
   );
 };
 
