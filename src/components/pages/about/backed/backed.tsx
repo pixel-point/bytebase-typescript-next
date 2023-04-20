a6d292672b8d5763e33eb73d7d03051e2e3f1094
lenaryan <alissadyiphil@gmail.com>
2023-04-20 16:49:50 +0600

    fix: add grid to markup

--- src/components/pages/about/backed/backed.tsx
+++ src/components/pages/about/backed/backed.tsx
@@ -1,36 +1,34 @@
 import Image from 'next/image';
 
 const Backed = () => (
-  <section className="container mt-40">
-    <span className="mx-auto block w-fit rounded-[20px] bg-secondary-1 py-2 px-2.5 text-12 font-bold uppercase leading-none tracking-wider">
+  <section className="container gap-x-grid mt-40 grid grid-cols-12 sm:grid-cols-4">
+    <span className="col-span-12 block w-fit justify-self-center rounded-[20px] bg-secondary-1 py-2 px-2.5 text-12 font-bold uppercase leading-none tracking-wider">
       Our Partners
     </span>
-    <h2 className="mt-5 text-center font-title text-88 font-semibold leading-none">
+    <h2 className="col-span-12 mt-5 justify-self-center font-title text-88 font-semibold leading-none">
       Backed by the best
     </h2>
-    <div className="mt-14 grid grid-cols-2 gap-x-10">
-      <div className="grid place-items-center border border-gray-90 bg-gray-97 py-[91px]">
-        <Image
-          src="/images/page/about/matrix-logo.svg"
-          alt="Matrix Partners"
-          width={353}
-          height={142}
-        />
-      </div>
-      <div className="flex items-center border border-gray-90">
-        <Image
-          src="/images/page/about/cto.webp"
-          alt=""
-          width={308}
-          height={306}
-          className="self-start"
-        />
-        <div>
-          <p className="text-18 font-semibold leading-normal text-primary-1">
-            Co-Founder & CTO - PingCAP
-          </p>
-          <p className="mt-2 font-title text-56 font-semibold leading-none">Dongxu Huang</p>
-        </div>
+    <div className="col-span-6 row-start-3 mt-14 grid place-items-center border border-gray-90 bg-gray-97 py-[91px]">
+      <Image
+        src="/images/page/about/matrix-logo.svg"
+        alt="Matrix Partners"
+        width={353}
+        height={142}
+      />
+    </div>
+    <div className="col-span-6 row-start-3 mt-14 flex items-center border border-gray-90">
+      <Image
+        src="/images/page/about/cto.webp"
+        alt=""
+        width={308}
+        height={306}
+        className="self-start"
+      />
+      <div>
+        <p className="text-18 font-semibold leading-normal text-primary-1">
+          Co-Founder & CTO - PingCAP
+        </p>
+        <p className="mt-2 font-title text-56 font-semibold leading-none">Dongxu Huang</p>
       </div>
     </div>
   </section>
--- src/components/pages/about/hero/hero.tsx
+++ src/components/pages/about/hero/hero.tsx
@@ -7,22 +7,21 @@ const Hero = () => {
     'Bytebase supports every major database systems, Bytebase offers the most comprehensive SQL Lint rules, All Bytebase source code is available on GitHub';
 
   return (
-    <section className="container pt-[136px] text-center">
+    <section className="container gap-x-grid grid grid-cols-12 justify-items-center pt-[136px] sm:grid-cols-4">
       <h1 className="sr-only">{title}</h1>
-      <span className="w-fit rounded-[20px] bg-secondary-1 py-2 px-2.5 text-12 font-bold uppercase leading-none tracking-wider">
+      <span className="col-span-12 w-fit rounded-[20px] bg-secondary-1 py-2 px-2.5 text-12 font-bold uppercase leading-none tracking-wider">
         {supTitle}
       </span>
-      <h2 className="mt-5 font-title text-88 font-semibold leading-none">
-        Simplify database changes
-        <br className="xs:hidden" />
-        with <mark className="bg-transparent text-primary-1">standardization</mark>, lint rules
+      <h2 className="col-span-12 mt-5 max-w-[1000px] text-center font-title text-88 font-semibold leading-none">
+        Simplify database changes with{' '}
+        <mark className="bg-transparent text-primary-1">standardization</mark>, lint rules
       </h2>
       <Image
         src="/images/page/about/why-we-build.png"
         alt={imgAlt}
         width={1292}
         height={631}
-        className="mx-auto mt-[52px]"
+        className="col-span-12 mt-[52px]"
         priority
       />
     </section>
