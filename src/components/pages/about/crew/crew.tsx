60da28fed72d8b14c3045d29b3467a17fce344b9
lenaryan <alissadyiphil@gmail.com>
2023-04-21 11:35:45 +0600

    fix: change crew markup to 12 column grid

--- src/components/pages/about/crew/crew.tsx
+++ src/components/pages/about/crew/crew.tsx
@@ -6,15 +6,15 @@ import { crewList } from './data';
 
 const Crew = () => (
   <section className="container mt-20 border-t border-gray-90 pt-[52px]">
-    <ul className="grid grid-cols-4 gap-x-10 gap-y-20">
+    <ul className="gap-x-grid grid grid-cols-12 gap-y-20">
       {crewList.map((member) => (
-        <li className="flex flex-col items-center justify-center" key={member.name}>
+        <li className="col-span-3 flex flex-col items-center justify-center" key={member.name}>
           <Image src={member.image} alt="" width={244} height={244} />
           <p className="text-24 font-bold leading-none">{member.name}</p>
-          <p className="mt-1 text-20 leading-7">{member.position}</p>
+          <p className="mt-1 text-center text-20 leading-7">{member.position}</p>
         </li>
       ))}
-      <li className="flex flex-col items-center justify-center">
+      <li className="col-span-3 flex flex-col items-center justify-center">
         <Image src="/images/page/about/join.png" alt="" width={244} height={244} />
         <LinkUnderlined href="/jobs">Join us</LinkUnderlined>
       </li>
