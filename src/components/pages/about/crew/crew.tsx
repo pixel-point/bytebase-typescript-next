280c88cf7f6fb10241b80951738f4c98bc29c9dd
lenaryan <alissadyiphil@gmail.com>
2023-04-21 16:00:38 +0600

    fix: change key for crew mapping

--- src/components/pages/about/crew/crew.tsx
+++ src/components/pages/about/crew/crew.tsx
@@ -7,8 +7,8 @@ import { crewList } from './data';
 const Crew = () => (
   <section className="container mt-20 border-t border-gray-90 pt-[52px]">
     <ul className="gap-x-grid grid grid-cols-12 gap-y-20">
-      {crewList.map((member) => (
-        <li className="col-span-3 flex flex-col items-center justify-center" key={member.name}>
+      {crewList.map((member, index) => (
+        <li className="col-span-3 flex flex-col items-center justify-center" key={index}>
           <Image src={member.image} alt="" width={244} height={244} />
           <p className="text-24 font-bold leading-none md:text-20 sm:text-18">{member.name}</p>
           <p className="mt-1 text-center text-20 leading-7">{member.position}</p>
