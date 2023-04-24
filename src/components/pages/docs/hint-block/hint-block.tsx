bd8598d3b18c515beaefe8c802fa0a8c8dbde38f
Thi Van Le <vannnyle@gmail.com>
2023-04-24 09:17:06 +0200

    feat: adjust styles

--- src/components/pages/docs/hint-block/hint-block.tsx
+++ src/components/pages/docs/hint-block/hint-block.tsx
@@ -30,17 +30,18 @@ const icons = {
 
 const HintBlock = ({ type = 'info', children }: HintBlockProps) => {
   const Icon = icons[type];
+
   return (
-    <figure
-      className={clsx(
-        'hint-block border shadow-[inset_6px_6px_0_#fff,0_5px_15px_rgba(172,178,210,0.5)]',
-        typeClassNames[type].wrapper,
-      )}
-    >
-      <div className={clsx('flex gap-x-4 px-3 pt-4 pb-8 md:gap-x-3', typeClassNames[type].inner)}>
+    <figure className={clsx('hint-block border ', typeClassNames[type].wrapper)}>
+      <div
+        className={clsx(
+          'flex gap-x-4 px-5 pt-6 pb-8 shadow-[inset_6px_6px_0_#fff,0_5px_15px_rgba(172,178,210,0.5)] md:gap-x-3 md:px-4 md:pt-5 md:pb-7 sm:px-3 sm:pt-4 sm:pb-5',
+          typeClassNames[type].inner,
+        )}
+      >
         <span
           className={clsx(
-            'flex h-8 w-8 items-center justify-center rounded-full shrink-0 md:w-7 md:h-7 sm:h-6 sm:w-6',
+            'flex h-8 w-8 shrink-0 items-center justify-center rounded-full md:h-7 md:w-7 sm:h-6 sm:w-6',
             typeClassNames[type].icon,
           )}
         >
@@ -49,13 +50,13 @@ const HintBlock = ({ type = 'info', children }: HintBlockProps) => {
 
         <div>
           <div className="not-prose">
-            <h4 className="text-24 font-bold leading-extra-tight tracking-tight text-gray-15 first-letter:capitalize">
+            <h4 className="text-24 font-bold leading-extra-tight tracking-tight text-gray-15 first-letter:capitalize lg:text-20 md:text-18 sm:pt-1.5 sm:text-14">
               {type}
             </h4>
           </div>
           <div
             className={clsx(
-              'prose mt-4 !text-15 !leading-snug prose-p:my-2.5 prose-p:first:mt-0 prose-p:last:mb-0',
+              'prose mt-4 space-y-2.5 !text-15 !leading-snug prose-p:my-2.5 prose-p:first:mt-0 prose-p:last:mb-0 sm:mt-3 sm:!text-14 sm:leading-snug',
               typeClassNames[type].text,
             )}
           >
