da5af9e4c4c668e75c5462b1cdec3d401b6dc15b
americano98 <mr.nik.bazarov@gmail.com>
2023-04-25 00:55:05 +0300

    feat: add blockquote to content

--- src/components/shared/content/content.tsx
+++ src/components/shared/content/content.tsx
@@ -1,12 +1,16 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import Image from 'next/image';
 
+import { ReactNode } from 'react';
+
 import clsx from 'clsx';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings/lib';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
 
 import CodeBlock from '@/components/shared/code-block';
+
+import QuoteIcon from '@/svgs/quote.inline.svg';
 
 import DocLinkBlock from './doc-link-block';
 import HintBlock from './hint-block';
@@ -41,6 +45,14 @@ const components = {
         style={{ width: '100%', height: '100%' }}
         alt={alt}
       />
+    );
+  },
+  blockquote: ({ children }: { children: ReactNode }) => {
+    return (
+      <blockquote>
+        <QuoteIcon className="mb-6 h-10 lg:mb-5 lg:h-9 sm:mb-2 sm:h-7" />
+        {children}
+      </blockquote>
     );
   },
   HintBlock,
