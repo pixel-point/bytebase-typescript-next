84e72b67cf9fc583debfee19190bcacc81d37def
americano98 <mr.nik.bazarov@gmail.com>
2023-04-25 12:15:26 +0300

    refactor: link for what is Bytebase

--- src/components/pages/blog/aside/what-is-bytebase/what-is-bytebase.tsx
+++ src/components/pages/blog/aside/what-is-bytebase/what-is-bytebase.tsx
@@ -1,10 +1,13 @@
-import Button from '@/components/shared/button';
+import Link from '@/components/shared/link';
 
 import ROUTE from '@/lib/route';
 
 const WhatIsBytebase = () => {
   return (
-    <figure className="flex flex-col justify-between rounded-xl bg-primary-1 p-5 text-white lg:col-span-5 lg:col-start-2 md:col-span-6 sm:col-span-full sm:rounded-[4px]">
+    <Link
+      className="flex flex-col justify-between rounded-xl bg-primary-1 p-5 text-white hover:bg-primary-2 lg:col-span-5 lg:col-start-2 md:col-span-6 sm:col-span-full sm:rounded-[4px]"
+      href={ROUTE.ABOUT}
+    >
       <div>
         <h3 className="font-title text-34 leading-none">What is Bytebase?</h3>
         <p className="mt-3 text-15 leading-snug lg:mt-2.5 sm:mt-2">
@@ -12,15 +15,10 @@ const WhatIsBytebase = () => {
           lifecycle of application database schemas.
         </p>
       </div>
-      <Button
-        className="mt-4 flex h-9 w-[122px] items-center justify-center uppercase"
-        theme="gray-filled"
-        href={ROUTE.ABOUT}
-        size="sm"
-      >
+      <span className="mt-4 flex h-9 w-[122px] items-center justify-center rounded-full bg-gray-15 text-13 font-bold uppercase tracking-wide text-white">
         Learn more
-      </Button>
-    </figure>
+      </span>
+    </Link>
   );
 };
 
