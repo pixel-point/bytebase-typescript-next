4fc234aefa291c25bbdf0a1ef00906624853c6ce
Thi Van Le <vannnyle@gmail.com>
2023-04-25 17:21:26 +0200

    feat: integrate form with segment

--- src/components/shared/subscription-form/subscription-form.tsx
+++ src/components/shared/subscription-form/subscription-form.tsx
@@ -3,16 +3,25 @@
 import Image from 'next/image';
 import NextLink from 'next/link';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
+import useSegment from '@/hooks/use-segment';
 import useIntersectionObserver from '@react-hook/intersection-observer';
 import { Alignment, Fit, Layout, useRive, useStateMachineInput } from '@rive-app/react-canvas';
 import clsx from 'clsx';
 
 import Route from '@/lib/route';
 
+const EMAIL_REGEX =
+  // eslint-disable-next-line no-control-regex, no-useless-escape
+  /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
 const SubscriptionForm = ({ className }: { className?: string }) => {
   const ref = useRef(null);
+  const [email, setEmail] = useState('');
+  const [formState, setFormState] = useState('default');
+  const [errorMessage, setErrorMessage] = useState('');
+  const { analytics } = useSegment();
 
   const { isIntersecting } = useIntersectionObserver(ref);
 
@@ -36,6 +45,47 @@ const SubscriptionForm = ({ className }: { className?: string }) => {
       rive.play();
     }
   }, [rive, isIntersecting]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.currentTarget.value.trim());
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!email) {
+      setErrorMessage('Please enter your email');
+      setFormState('error');
+    } else if (!EMAIL_REGEX.test(email)) {
+      setErrorMessage('Please enter a valid email');
+      setFormState('error');
+    } else {
+      setFormState('loading');
+      setErrorMessage('');
+
+      if (analytics) {
+        // Manually updating user subscription status to re-subscribe
+        // Doc: https://segment.com/docs/connections/destinations/catalog/mailchimp/#manually-updating-user-subscription-status
+        analytics.identify(
+          email,
+          {
+            category: 'subscribe-newsletter',
+          },
+          {
+            integrations: {
+              MailChimp: {
+                subscriptionStatus: 'subscribed',
+              },
+            },
+          },
+        );
+
+        setTimeout(() => {
+          setFormState('success');
+        }, 1000);
+      }
+    }
+  };
+
   return (
     <section
       className={clsx(
@@ -60,18 +110,29 @@ const SubscriptionForm = ({ className }: { className?: string }) => {
           />
         </div>
         <div className="col-span-5 col-start-7 xl:col-span-6 xl:col-start-7 md:col-span-6 md:col-start-7  sm:max-w-[80%] 2xs:max-w-none">
-          <form className="-ml-10 pt-16 pb-[75px] 3xl:ml-0 xl:-ml-6 xl:pb-[60px] xl:pt-[51px] md:ml-0 md:pt-10 md:pb-11 sm:pt-9 sm:pb-11">
+          <form
+            className="-ml-10 pt-16 pb-[75px] 3xl:ml-0 xl:-ml-6 xl:pb-[60px] xl:pt-[51px] md:ml-0 md:pt-10 md:pb-11 sm:pt-9 sm:pb-11"
+            noValidate
+            onSubmit={handleSubmit}
+          >
             <h3 className="font-title text-88 font-semibold leading-none xl:text-68 xl:leading-104 md:text-56 md:leading-none sm:text-48 sm:leading-95">
               Subscribe to&nbsp;Newsletter
             </h3>
             <p className="relative mt-9 flex h-16 xl:mt-6 xl:h-12 md:mt-4 sm:mt-5">
               <input
                 className="remove-autocomplete-styles flex-grow rounded-l-full py-6 px-7 text-16 leading-none tracking-tight text-gray-15 placeholder-gray-40 outline-none xl:py-4 xl:px-5 sm:px-5"
-                type="text"
+                type="email"
+                name="email"
+                autoComplete="email"
+                value={email}
                 placeholder="Your email address..."
                 onInput={() => input?.fire()}
+                onChange={handleInputChange}
               />
-              <button className="flex-shrink-0 rounded-r-full bg-black py-6 px-11 text-16 font-bold uppercase leading-none hover:bg-[#17225B] xl:py-4 md:py-3 md:px-5 sm:px-5 sm:py-3">
+              <button
+                className="flex-shrink-0 rounded-r-full bg-black py-6 px-11 text-16 font-bold uppercase leading-none hover:bg-[#17225B] xl:py-4 md:py-3 md:px-5 sm:px-5 sm:py-3"
+                type="submit"
+              >
                 <span className="md:hidden">Subscribe</span>{' '}
                 <img
                   src="/images/arrow-form.svg"
