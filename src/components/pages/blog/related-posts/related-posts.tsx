cb821f5263d0eaa50f794b4a844ba394e447a476
americano98 <mr.nik.bazarov@gmail.com>
2023-04-25 11:44:40 +0300

    feat: add margins, refactor arrow

--- src/components/pages/blog/related-posts/related-posts.tsx
+++ src/components/pages/blog/related-posts/related-posts.tsx
@@ -4,29 +4,30 @@ import { BlogPost } from '@/types/blog-post';
 
 import ROUTE from '@/lib/route';
 
+import Arrow from '@/svgs/arrow.inline.svg';
+
 import BlogPostCard from '../blog-post-card';
 
 type RelatedPostsProps = {
   posts: BlogPost[];
 };
 
-// TODO: fix button arrow
 const RelatedPosts = ({ posts }: RelatedPostsProps) => {
   if (!posts) return null;
 
   return (
-    <section className="container related-posts mt-10">
+    <section className="container related-posts mt-20 lg:mt-14 md:mt-10 sm:mt-8">
       <div className="flex items-center justify-between border-t border-gray-90 pt-16 lg:pt-8 md:pt-10 sm:pt-8">
         <h2 className="font-title text-52 leading-none lg:text-50 md:text-46 sm:text-34">
           Related posts
         </h2>
         <Link
-          className="flex items-center rounded-full bg-gray-97 px-5 py-[11px] text-18 font-medium leading-none 
-                      md:text-16 sm:py-2 sm:px-4 sm:text-14"
+          className="flex items-center gap-x-2.5 rounded-full bg-gray-97 px-5 py-[11px] text-18 font-medium leading-none hover:bg-gray-90 
+                      md:text-16 sm:gap-x-2 sm:py-2 sm:px-4 sm:text-14"
           href={ROUTE.BLOG}
-          withArrow
         >
           Back to blog
+          <Arrow className="w-5 sm:w-4" />
         </Link>
       </div>
       <ul className="gap-x-grid mt-10 grid grid-cols-12 lg:mt-8 md:mt-6 md:gap-y-8 sm:gap-y-8">
