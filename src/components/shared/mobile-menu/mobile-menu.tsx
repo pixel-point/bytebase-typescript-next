b459e71bec503550cbffa193b0da702ae0576dc3
americano98 <mr.nik.bazarov@gmail.com>
2023-04-27 20:00:44 +0300

    refactor: blank, solve console log prefetch true

--- src/components/shared/mobile-menu/mobile-menu.tsx
+++ src/components/shared/mobile-menu/mobile-menu.tsx
@@ -140,7 +140,7 @@ const MobileMenu = () => {
                         <Link
                           className="flex w-full flex-col items-start justify-center whitespace-nowrap py-5 text-20 font-medium leading-none tracking-tight transition-colors duration-200 hover:cursor-pointer"
                           href={href}
-                          prefetch={href !== Route.BLOG}
+                          prefetch={href !== Route.BLOG ? false : undefined}
                           onClick={toggleMenu}
                         >
                           {title}
