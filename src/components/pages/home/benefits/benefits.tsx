730f70a78866ca1deb7608068a826b4c69de2f35
Sasha Sushko <sasha@sushko.me>
2023-05-01 10:17:35 +0300

    fix: remove benefits gradient bg

--- src/components/pages/home/benefits/benefits.tsx
+++ src/components/pages/home/benefits/benefits.tsx
@@ -43,9 +43,9 @@ const Benefits = () => {
         {benefits.map(({ title, description, image }, idx) => (
           <li
             key={idx}
-            className={clsx({
-              'bg-radial-blue': openedItemIdx === idx,
-              'cursor-pointer bg-tones-purple-light': openedItemIdx !== idx,
+            className={clsx('bg-tones-purple-light', {
+              'bg-opacity-20': openedItemIdx === idx,
+              'cursor-pointer': openedItemIdx !== idx,
             })}
             onClick={() => setOpenedItemIdx(idx)}
           >
