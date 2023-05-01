eabe7dbcb0b7068d6c14fe4b012ee468fc587aa6
Sasha Sushko <sasha@sushko.me>
2023-05-01 09:47:53 +0300

    fix: hero card size

--- src/components/pages/home/hero/card/card.tsx
+++ src/components/pages/home/hero/card/card.tsx
@@ -48,11 +48,11 @@ const Card = ({ color, className, cover, videos, title, href, description }: Car
           />
         </div>
       </div>
-      <div className="invisible translate-y-24 opacity-0 transition-all duration-700 group-[.done]:visible group-[.done]:translate-y-0 group-[.done]:opacity-100 sm:visible sm:translate-y-0 sm:opacity-100 sm:transition-none">
-        <h3 className="mt-8 overflow-hidden text-ellipsis whitespace-nowrap text-36 font-bold leading-extra-tight tracking-tighter xl:mt-6 xl:text-32 md:mt-5 md:text-30">
+      <div className="invisible mt-8 min-h-[194px] translate-y-24 opacity-0 transition-all duration-700 group-[.done]:visible group-[.done]:translate-y-0 group-[.done]:opacity-100 xl:mt-6 xl:min-h-[172px] md:mt-5 md:min-h-[144px] sm:visible sm:translate-y-0 sm:opacity-100 sm:transition-none">
+        <h3 className="overflow-hidden text-ellipsis whitespace-nowrap text-36 font-bold leading-extra-tight tracking-tighter xl:text-32 md:text-30">
           {title}
         </h3>
-        <p className="mt-4 h-[90px] w-3/4 text-20 leading-normal 3xl:w-full xl:relative xl:mt-3 xl:h-[81px] xl:overflow-hidden xl:text-18 xl:after:absolute xl:after:bottom-0 xl:after:right-0 xl:after:h-[30px] xl:after:w-2/3 xl:after:bg-gradient-to-r xl:after:from-transparent xl:after:to-white md:mt-1.5 md:h-[72px] md:text-16 sm:h-auto sm:leading-snug sm:after:hidden">
+        <p className="mt-4 w-3/4 text-20 leading-normal 3xl:w-full xl:relative xl:mt-3 xl:overflow-hidden xl:text-18 xl:after:absolute xl:after:bottom-0 xl:after:right-0 xl:after:h-[30px] xl:after:w-2/3 xl:after:bg-gradient-to-r xl:after:from-transparent xl:after:to-white md:mt-1.5 md:text-16 sm:h-auto sm:leading-snug sm:after:hidden">
           {description}
         </p>
         <LinkUnderlined href={href} className="mt-6 xl:mt-5 md:mt-3">
