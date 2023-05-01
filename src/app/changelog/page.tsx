5df923ce20778c7f11fc19648398f6ed217a8431
americano98 <mr.nik.bazarov@gmail.com>
2023-05-01 12:23:44 +0300

    feat: add seo data for changelog root

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -1,8 +1,13 @@
 import { notFound } from 'next/navigation';
 
+import getMetadata from '@/utils/get-metadata';
+
 import Posts from '@/components/pages/changelog/posts';
 
 import { getChangelogPostsPerPage } from '@/lib/api-changelog';
+import SEO_DATA from '@/lib/seo-data';
+
+export const metadata = getMetadata(SEO_DATA.CHANGELOG);
 
 export default function ChangelogPage() {
   const data = getChangelogPostsPerPage(1);
