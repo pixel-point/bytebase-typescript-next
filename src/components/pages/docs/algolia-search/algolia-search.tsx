57076264fd721dcd2162abc1285429655253a07b
americano98 <mr.nik.bazarov@gmail.com>
2023-05-08 11:42:26 +0300

    feat: set next link for hit

--- src/components/pages/docs/algolia-search/algolia-search.tsx
+++ src/components/pages/docs/algolia-search/algolia-search.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { ReactNode } from 'react';
+
 import { DocSearch } from '@docsearch/react';
+
+import Link from '@/components/shared/link';
 
 const AlgoliaSearch = ({ className }: { className?: string }) => {
   return (
@@ -10,8 +14,17 @@ const AlgoliaSearch = ({ className }: { className?: string }) => {
         indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!}
         apiKey={process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!}
         placeholder="What you need?"
+        hitComponent={Hit}
       />
     </div>
+  );
+};
+
+const Hit = ({ hit, children }: { hit: any; children: ReactNode }) => {
+  return (
+    <Link href={hit.url} className="DocSearch-Hit--Child">
+      {children}
+    </Link>
   );
 };
 
