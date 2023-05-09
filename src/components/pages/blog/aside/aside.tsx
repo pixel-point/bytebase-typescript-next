89802b3449c3d2982dbb9e1191f0bfa35a654f38
Tianzhou Chen <t@bytebase.com>
2023-05-10 00:17:08 +0800

    chore: hide static author section

--- src/components/pages/blog/aside/aside.tsx
+++ src/components/pages/blog/aside/aside.tsx
@@ -14,7 +14,7 @@ const Aside = ({ author, tocItems }: AsideProps) => {
   return (
     <aside className="scrollbar-hidden lg:gap-x-grid col-span-3 ml-auto flex w-full flex-col gap-y-9 overflow-y-auto lg:static lg:col-span-full lg:mt-14 lg:grid lg:grid-cols-12 md:mt-10 sm:mt-8 sm:gap-y-4">
       <WhatIsBytebase />
-      <Author author={author} />
+      {/* <Author author={author} /> */}
       {tocItems.length > 0 && <TableOfContents items={tocItems} />}
     </aside>
   );
